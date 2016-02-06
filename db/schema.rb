ce608829bcde6b071a055e2128edd1065fb918c1
Wil Burke <billyburke13@gmail.com>
2016-02-06 17:03:28 -0500

    removing Qs

--- db/schema.rb
+++ db/schema.rb
@@ -36,14 +36,6 @@ ActiveRecord::Schema.define(version: 20160206205702) do
   add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
   add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
-  create_table "questions", force: :cascade do |t|
-    t.string   "title"
-    t.text     "body"
-    t.boolean  "resolved"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "topics", force: :cascade do |t|
     t.string   "name"
     t.boolean  "public",      default: true

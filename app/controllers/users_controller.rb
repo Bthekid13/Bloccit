c05c3dee7b381f3272e49e011890688c2d153873
Wil Burke <billyburke13@gmail.com>
2016-02-14 12:08:06 -0500

    user controller update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,20 +22,16 @@ class UsersController < ApplicationController
     end
   end
 
-  def update
-
-  end
-
-  def destroy
-
-  end
-
   def confirm
     @user = User.new
     @user.name = params[:user][:name]
     @user.email = params[:user][:email]
     @user.password = params[:user][:password]
     @user.password_confirmation = params[:user][:password_confirmation]
-
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
+  
 end

d217f5502437a56e6160be958da7aaea142db5a0
Wil Burke <billyburke13@gmail.com>
2016-02-14 16:07:53 -0500

    g

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,10 +28,5 @@ class UsersController < ApplicationController
     @user.email = params[:user][:email]
     @user.password = params[:user][:password]
     @user.password_confirmation = params[:user][:password_confirmation]
-  end
-
-  def show
-    @user = User.find(params[:id])
-  end
-  
+  end  
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -61,24 +61,4 @@ RSpec.describe UsersController, type: :controller do
       expect(session[:user_id]).to eq assigns(:user).id
     end
   end
-
-  describe "not signed in" do
-    let(:factory_user) {build(:user)}
-    before do
-      post :create, :user: new_user_attributes
-    end
-    it "returns http success" do
-      get :show, {id: factory_user.id}
-      expect(response).to have_http_status(:success)
-    end
-
-    it "renders the #show view" do
-      get :show, {id: factory_user.id}
-      expect(response).to render_template :show
-    end
-
-    it "assigns factory_user to @user" do
-      get :show, {id: factory_user.id}
-    end
-  end
 end

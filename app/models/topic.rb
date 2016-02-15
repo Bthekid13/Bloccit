0b2a489639f4785661c5174e881b6e0d6098af7c
Wil Burke <billyburke13@gmail.com>
2016-02-15 17:10:28 -0500

    added public/private scope

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -7,7 +7,9 @@ class Topic < ActiveRecord::Base
   validates :name, length: {minimum: 5}, presence: true
   validates :description, length: {minimum: 10}, presence: true
 
-  scope :visible_to, -> (user) { user ? all : where(public: true) }
+  scope :privately_viewable, -> {where(public: false)}
+  scope :publically_viewable, -> {where(public: true)}
+  scope :visible_to, -> (user) { user ? all : publically_viewable }
 
 
 end
--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -27,4 +27,38 @@ RSpec.describe Topic, type: :model do
       expect(topic.public).to be(true)
     end
   end
+
+  describe "scopes" do
+    before do
+      # #1
+      @public_topic = Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph)
+      @private_topic = Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph, public: false)
+    end
+
+    describe "visible_to(user)" do
+      it "returns all topics if the user is present" do
+        user = User.new
+        # #2
+        expect(Topic.visible_to(user)).to eq(Topic.all)
+      end
+
+      it "returns only public topics if user is nil" do
+        # #3
+        expect(Topic.visible_to(nil)).to eq([@public_topic])
+      end
+    end
+    describe "publically_viewable" do
+      it "returns only public topics" do
+        user = User.new
+        expect(Topic.publically_viewable).to eq(@public_topic)
+      end
+    end
+
+    describe "publically_viewable" do
+      it "returns only private topics" do
+        user = User.new
+        expect(Topic.privately_viewable).to eq(@private_topic)
+      end
+    end
+  end
 end

5c4113550ea2b688503fee1d4286aac55210d022
Wil Burke <billyburke13@gmail.com>
2016-02-15 17:13:38 -0500

    g

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -7,5 +7,7 @@ class Topic < ActiveRecord::Base
   validates :name, length: {minimum: 5}, presence: true
   validates :description, length: {minimum: 10}, presence: true
 
+  scope :visible_to, -> (user) { user ? all : where(public: true) }
+
 
 end

480d32845d409a0c48389dc034a9d22ffbb9706d
Wil Burke <billyburke13@gmail.com>
2016-02-16 22:00:02 -0500

    checkout

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -4,14 +4,19 @@ class VotesController < ApplicationController
 
   def up_vote
     update_vote(1)
-    redirect_to :back
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
   def down_vote
     update_vote(-1)
-    redirect_to :back
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
-end
 
   private
 
@@ -25,3 +30,4 @@ end
       @vote = current_user.votes.create(value: new_value, post: @post)
     end
   end
+end

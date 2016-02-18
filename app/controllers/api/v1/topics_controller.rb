806b6c7ad3848db0841ca0f2804a92bc2986e660
Wil Burke <billyburke13@gmail.com>
2016-02-17 22:28:46 -0500

    added posts/comments controllers for my api + child posts/comments

--- app/controllers/api/v1/topics_controller.rb
+++ app/controllers/api/v1/topics_controller.rb
@@ -5,12 +5,12 @@ class Api::V1::TopicsController < Api::V1::BaseController
   def index
     topics = Topics.all
 
-    render json: topic.to_json, status: 200
+    render json: topics.to_json(include: :posts), status: 200
   end
 
   def show
     topic = Topic.find(params[:id])
 
-    render json: topic.to_json, status: 200
+    render json: topic.to_json(include: :posts), status: 200
   end
 end

503470fc70864e21e0b5f8a4692384f59b33886f
Wil Burke <billyburke13@gmail.com>
2016-02-20 14:15:51 -0500

    removing assignment info from checkpoints

--- app/models/advertisement.rb
+++
@@ -1,2 +0,0 @@
-class Advertisement < ActiveRecord::Base
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -21,17 +21,6 @@ posts = Post.all
   )
 end
 
-#Creates Advertisements
-10.times do
-  Advertisement.create!(
-
-  title: RandomData.random_sentence,
-  copy: RandomData.random_paragraph,
-  price: 99
-  
-  )
-end
-
 
 puts "Seed finished"
 puts "#{Post.count} posts were created"

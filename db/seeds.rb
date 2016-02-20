54ff7441f273d155cd036a712940ecdc3c21990e
Wil Burke <billyburke13@gmail.com>
2016-02-20 15:27:31 -0500

    clean

--- db/seeds.rb
+++ db/seeds.rb
@@ -35,27 +35,9 @@ posts = Post.all
   )
 end
 
-#Creates Advertisements
-# 10.times do
-#   Advertisement.create!(
-#
-#   title: RandomData.random_sentence,
-#   copy: RandomData.random_paragraph,
-#
-#   )
-# end
 
-#Creates Q's
-5.times do
-  Question.create!(
-  title: RandomData.random_sentence,
-  body: RandomData.random_paragraph
-
-  )
-end
 
 puts "Seed finished"
 puts "#{Topic.count} topics were created"
 puts "#{Post.count} posts were created"
 puts "#{Comment.count} comments were created"
-puts "#{Question.count} Q's were asked"

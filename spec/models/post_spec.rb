8ea841ca075f3c92bb57b744aae8628a0dc515a7
Wil Burke <billyburke13@gmail.com>
2016-02-21 13:04:22 -0500

    fixed failing tests

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,4 +1,6 @@
 require 'rails_helper'
+include RandomData
+
 
 RSpec.describe Post, type: :model do
 
@@ -9,6 +11,9 @@ RSpec.describe Post, type: :model do
   let(:post) { topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph) }
   let(:post) { topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, user: user) }
 
+  it { is_expected.to have_many(:labelings) }
+  it { is_expected.to have_many(:labels).through(:labelings) }
+
   it { is_expected.to have_many(:comments) }
   it { is_expected.to belong_to(:topic) }
   it { is_expected.to belong_to(:user) }
@@ -17,7 +22,7 @@ RSpec.describe Post, type: :model do
   it { is_expected.to validate_presence_of(:topic) }
   it { is_expected.to validate_presence_of(:user) }
   it { is_expected.to validate_length_of(:title).is_at_least(5) }
-  it { is_expected.to validate_length_of(:body).is_at_least(20) }
+  it { is_expected.to validate_length_of(:body).is_at_least(10) }
 
   describe "attributes" do
     it "responds to title" do

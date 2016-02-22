7173750c9480ccc85590088b3bf85ff38e4b329a
Wil Burke <billyburke13@gmail.com>
2016-02-21 22:20:12 -0500

    g

--- spec/controllers/votes_controller_spec.rb
+++ spec/controllers/votes_controller_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe VotesController, type: :controller do
   let(:my_topic) { create(:topic) }
   let(:my_user) { create(:user) }
   let(:other_user) { create(:user) }
-  let(:user_post) { build(:post, topic: my_topic, user: other_user) }
+  let(:user_post) { create(:post, topic: my_topic, user: other_user) }
   let(:my_vote) { Vote.create!(value: 1) }
 
   # #17

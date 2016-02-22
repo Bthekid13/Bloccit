1af6c8c383c7beb63d0584d5c0edd94016f5b66c
Wil Burke <billyburke13@gmail.com>
2016-02-21 22:23:07 -0500

    f

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,23 +1,25 @@
 require 'rails_helper'
+include RandomData
 
 RSpec.describe Vote, type: :model do
 
-   let(:topic) { create(:topic) }
+  let(:topic) { create(:topic) }
    let(:user) { create(:user) }
    let(:post) { create(:post) }
-   let(:vote)  { Vote.create!(value: 1, post: post, user: user) }
+  let(:vote)  { Vote.create!(value: 1, post: post, user: user) }
 
-    it { is_expected.to belong_to(:post) }
-    it { is_expected.to belong_to(:user) }
+  it { is_expected.to belong_to(:post) }
+  it { is_expected.to belong_to(:user) }
 
-    it { is_expected.to validate_presence_of(:value) }
-    it { is_expected.to validate_inclusion_of(:value).in_array([-1, 1]) }
+  it { is_expected.to validate_presence_of(:value) }
+  it { is_expected.to validate_inclusion_of(:value).in_array([-1, 1]) }
 
   describe "update_post callback" do
     it "triggers update_post on save" do
       expect(vote).to receive(:update_post).at_least(:once)
+      vote.save
     end
-  
+
     it "update_post should call update rank on post" do
       expect(post).to receive(:update_rank).at_least(:once)
       vote.save

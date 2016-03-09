505416e4aa9274ed256270eb08eca4415cbfe7ae
Xander Miller <xander.miller@gmail.com>
2016-03-09 14:27:14 -0500

    fixes specs so they all pass

--- app/models/post.rb
+++ app/models/post.rb
@@ -11,7 +11,7 @@ class Post < ActiveRecord::Base
 
 
   validates :title, length: {minimum: 5}, presence: true
-  validates :body, length: {minimum: 20}, presence: true
+  validates :body, length: {minimum: 10}, presence: true
   validates :topic, presence: true
   validates :user, presence: true
 

1c7e024679b60819046e524bf6ac3251e0531e44
Xander Miller <xander.miller@gmail.com>
2016-03-16 16:05:44 -0400

    oops didn't save changes

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -25,15 +25,9 @@ RSpec.describe Topic, type: :model do
       expect(topic.public).to be(true)
     end
   end
-<<<<<<< HEAD
 
   describe "scopes" do
     before do
-      # #1
-=======
-  describe "scopes" do
-    before do
->>>>>>> xm_46_checkpoint_private
       @public_topic = Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph)
       @private_topic = Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph, public: false)
     end
@@ -41,16 +35,14 @@ RSpec.describe Topic, type: :model do
     describe "visible_to(user)" do
       it "returns all topics if the user is present" do
         user = User.new
-        # #2
         expect(Topic.visible_to(user)).to eq(Topic.all)
       end
 
       it "returns only public topics if user is nil" do
-        # #3
         expect(Topic.visible_to(nil)).to eq([@public_topic])
       end
     end
-<<<<<<< HEAD
+
     describe "publically_viewable" do
       it "returns only public topics" do
         user = User.new
@@ -64,7 +56,5 @@ RSpec.describe Topic, type: :model do
         expect(Topic.privately_viewable).to eq(@private_topic)
       end
     end
-=======
->>>>>>> xm_46_checkpoint_private
   end
 end
